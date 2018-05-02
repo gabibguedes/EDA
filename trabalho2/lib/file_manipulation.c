68131af163beebe685a6ed4e721af206ff39747f
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2018-05-02 16:01:10 -0300

    cometando o file manipulation
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- trabalho2/lib/file_manipulation.c
+++ trabalho2/lib/file_manipulation.c
@@ -4,15 +4,16 @@
 #include <stdlib.h>
 #include <time.h>
 
+//Função para montar o caminho do arquivo de acordo com o número aleatório
 void file_path(char *file_name_asphalt, char *file_name_grass, int number){
   char asphalt_file[] = "DataSet/asphalt/asphalt_";
   char grass_file[] = "DataSet/grass/grass_";
   char txt[] = ".txt\0";
   snprintf (file_name_asphalt, 100, "%s%02d%s", asphalt_file, number, txt );
   snprintf (file_name_grass, 100, "%s%02d%s", grass_file, number, txt );
-
 }
 
+//Monta vetor de números aleatórios para escolher os arquivos
 void chose_files(int *photos, int *rest){
   int i, j, k, unique, random_number;
 
@@ -37,15 +38,19 @@ void chose_files(int *photos, int *rest){
     }
   }
 
+  //Montando vetor de números restantes
   k=0;
   while(k < 25){
     for(i = 1; i < 51; i++){
       unique = 1;
       for(j=0; j<25; j++){
+        //Verifica s o número já foi escolhido
         if(photos[j] == i){
           unique = 0;
         }
       }
+      //Se o número não estiver no vetor de números aleatórios ele é adicionado
+      //ao vetor de números restantes
       if(unique){
         rest[k] = i;
         k++;
@@ -54,6 +59,7 @@ void chose_files(int *photos, int *rest){
   }
 }
 
+//Função de contar colunas
 int count_columns(char *file_name){
   int number, columns = 1, on_the_line = 1;
   char semicolon;
