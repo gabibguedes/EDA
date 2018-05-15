d801ace834e6049819ab21e477f55213cf7780c2
Felipe Campos <fepas.unb@gmail.com>
2018-05-15 14:46:35 -0300

    Adicionando trabalho 3

---
+++ trabalho3/projeto3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Contact {
+  char name[100];
+  char phone[10];
+  char address[100];
+  unsigned int cep;
+  char birthday[10];
+  int *next;
+  int *before;
+};
+
+int main(int argc, char const *argv[]) {
+
+
+
+  return 0;
+}
