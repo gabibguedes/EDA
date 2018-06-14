22b35d131b5436be3dc7644347035444131c4223
helenagoulart <helenabretasg@gmail.com>
2018-06-14 16:01:08 -0300

    Incluindo a funcao de imprimir

---
+++ trabalho5/imprime.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	
+	printf("     1\n");
+	printf("   /   \\ \n");
+	printf("  2      3\n");
+	printf(" / \\    / \\ \n");
+	printf("4   5   6  7\n");
+
+	return 0;
+}
